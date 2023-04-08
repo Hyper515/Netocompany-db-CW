a6084ec09b105620c79e5710e1ad843faad2dd8f
Hyper515 <ethan.akin16@outlook.com>
2023-04-08 14:54:05 +0100

    added application files to edit the patient table

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+
+const app = express();
+const db = new sqlite3.Database('patients.Vo2.db');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  db.all('SELECT * FROM Patient', (err, rows) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.render('index', { Patient: rows });
+    }
+  });
+});
+
+app.get('/search', (req, res) => {
+  const { query } = req.query;
+  db.all(`SELECT * FROM Patient WHERE ForeName LIKE '%${query}%'`, (err, rows) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.render('index', { Patient: rows });
+    }
+  });
+});
+
+app.get('/edit/:NhsNO', (req, res) => {
+  const { NhsNO } = req.params;
+  db.get('SELECT * FROM Patient WHERE NhsNO = ?', [NhsNO], (err, row) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.render('edit', { patient: row });
+    }
+  });
+});
+
+app.post('/edit/:NhsNO', (req, res) => {
+  const { NhsNO } = req.params;
+  const { ForeName, Dob, Gender, MobNo } = req.body;
+  db.run('UPDATE Patient SET ForeName = ?, Dob = ?, Gender = ?, MobNo = ? WHERE NhsNO = ?', [ForeName, Dob, Gender, MobNo, NhsNO], (err) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Server started on http://localhost:3001');
+});
